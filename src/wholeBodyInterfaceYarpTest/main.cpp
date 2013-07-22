4334553101adbc29c15c0665de4279f1e5304d24
andreadelprete <andre.delprete@gmail.com>
2013-07-22 16:53:01 +0200

    Add test

---
+++ src/wholeBodyInterfaceYarpTest/main.cpp
@@ -0,0 +1,63 @@
+/*
+ * Author: Andrea Del Prete.
+ * Copyright (C) 2007 The Robotcub consortium.
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+
+/**
+ * \infile Tests for wholeBodyInterfaceYarp.
+ */
+#include <yarp/os/Network.h>
+#include <yarp/os/Time.h>
+
+#include <yarp/math/Math.h>
+#include <yarp/sig/Vector.h>
+#include <yarp/math/Rand.h>
+
+#include <iCub/skinDynLib/common.h>
+
+#include <wbiy/wbiy.h>
+
+#include <stdio.h>
+#include <math.h>
+#include <string>
+
+#include <iostream>
+#include <typeinfo>
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::math;
+using namespace iCub::skinDynLib;
+using namespace std;
+using namespace wbi;
+using namespace wbiy;
+
+const double TOL = 1e-8;
+
+
+int main(int argc, char * argv[])
+{
+//    std::cout << "Int type id: "<< typeid(int).name() << std::endl;
+    Network yarp;
+    vector<string> bodyPartNames(BodyPart_s, BodyPart_s + sizeof(BodyPart_s) / sizeof(string) );
+    for(vector<string>::iterator it=bodyPartNames.begin(); it!=bodyPartNames.end(); it++)
+        printf("Body part %s\n", it->c_str());
+    iWholeBodySensors *is = new yarpWholeBodySensors("testSensInt", "icubSim", bodyPartNames);
+    is->addJoint(LocalId(LEFT_ARM, 0));
+    vector<int> jList(2);
+    jList[0] = 1;
+    jList[1] = 3;
+    is->addJoints(LocalIdList(LEFT_ARM, jList));
+    if(!is->init())
+    {
+        
+    }
+    printf("DoF = %d\n", is->getDoFs());
+    
+    printf("Main returning...\n");
+    return 0;
+}
+
+
