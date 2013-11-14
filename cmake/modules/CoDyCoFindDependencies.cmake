5bb4ce80e35cd63c49de77435ceb70ac3c0a97c3
Silvio Traversaro <silvio.traversaro@iit.it>
2013-11-14 12:53:07 +0900

    Added explicit find_package(orocos_kdl) for being sure

--- cmake/modules/CoDyCoFindDependencies.cmake
+++ cmake/modules/CoDyCoFindDependencies.cmake
@@ -1,3 +1,19 @@
+#### Find orocos_kdl, with fallback procedure to be compatible with ros installed version
+find_package(orocos_kdl)
+
+#support also for the old version of kdl cmake package
+if(NOT orocos_kdl_FOUND)
+   find_package(Orocos-KDL)
+   if(NOT Orocos-KDL_FOUND)
+      message(WARNING "KDL not found: neither orocos_kdl or Orocos-KDL cmake packages are available")
+   else(NOT Orocos-KDL_FOUND)
+      set(orocos_kdl_INCLUDE_DIRS ${Orocos-KDL_INCLUDE_DIRS})
+      set(orocos_kdl_LIBRARY_DIRS ${Orocos-KDL_LIBRARY_DIRS})
+      set(orocos_kdl_LIBRARIES ${Orocos-KDL_LIBRARIES})
+      set(orocos_kdl_FOUND true)
+   endif(NOT Orocos-KDL_FOUND)
+endif(NOT orocos_kdl_FOUND)
+
 #### Find kdl_codyco, require that kdl_codyco version is at least kdl_codyco_REQVERSION
 set(kdl_codyco_REQVERSION_MAJOR "0")
 set(kdl_codyco_REQVERSION_MINOR "0")
