87ac354b8de301c75975f87ccca2c3e399df11e7
Andrea Del Prete <andre.delprete@gmail.com>
2013-12-04 10:46:30 +0100

    Add empty files for recursive linear estimator

---
+++ src/modules/motorFrictionIdentification/include/motorFrictionIdentification/recursiveLinearEstimator.h
@@ -0,0 +1,27 @@
+/* 
+ * Copyright (C) 2013 CoDyCo
+ * Author: Andrea Del Prete
+ * email:  andrea.delprete@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef _RECURSIVE_LINEAR_ESTIMATOR
+#define _RECURSIVE_LINEAR_ESTIMATOR
+
+namespace motorFrictionIdentification
+{
+
+
+}   // end namespace 
+
+#endif
