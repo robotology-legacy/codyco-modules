314da04ae9b3fad44b5da7347b6fc2f55b7bb4f5
francesco-romano <francesco.romano.1987@gmail.com>
2014-02-24 12:18:45 +0100

    Changed CMakeLists to solve dependency order inclusion

---
+++ src/libraries/paramHelp/src/paramHelpUtil.cpp
@@ -0,0 +1,20 @@
+//
+//  paramHelpUtil.cpp
+//  CoDyCo
+//
+//  Created by Francesco Romano on 24/02/14.
+//
+//
+
+#include <paramHelp/paramHelpUtil.h>
+
+namespace paramHelp {
+
+    const char* PORT_IN_STREAM_SUFFIX    = "/stream:i";
+    const char* PORT_OUT_STREAM_SUFFIX   = "/stream:o";
+    const char* PORT_OUT_INFO_SUFFIX     = "/info:o";
+    const char* PORT_IN_INFO_SUFFIX      = "/info:i";
+    const char* PORT_RPC_SUFFIX          = "/rpc";
+    const char* PORT_OUT_MONITOR_SUFFIX  = "/monitor:o";
+
+}
