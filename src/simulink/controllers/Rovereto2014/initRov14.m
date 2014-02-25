ea023dfd57585a1cf4594230a918aaed833c7804
Daniele Pucci <daniele.pucci@iit.it>
2014-02-25 12:46:02 +0100

    Added variables to WBC blocks.

--- src/simulink/controllers/Rovereto2014/initRov14.m
+++ src/simulink/controllers/Rovereto2014/initRov14.m
@@ -2,9 +2,13 @@
 addpath(genpath('/home/daniele/MATLAB'))
 addpath(genpath('/home/daniele/src/codyco/build'))
 addpath(genpath('/home/daniele/src/codyco/src/simulink/controllers'))
-
+cd '/home/daniele/src/codyco/src/simulink/controllers/Rovereto2014'
 % Controller period
 Ts = 0.01;
+
+% Desired positions for postural task
+qDes = zeros(25,1);
+
 
 % Controller gains
 k = [ 1 1 0
