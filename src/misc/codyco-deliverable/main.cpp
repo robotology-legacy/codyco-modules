35a0e31a04fe81a34ea37b8c7107e431f84a5b10
francesco-romano <francesco.romano.1987@gmail.com>
2014-02-25 15:37:08 +0100

    Added codyco deliverable results and code

---
+++ src/misc/codyco-deliverable/main.cpp
@@ -0,0 +1,72 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include <stdio.h>
+#include <yarp/os/Network.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/os/Time.h>
+#include <yarp/sig/Vector.h>
+
+#include <string>
+
+using namespace yarp::dev;
+using namespace yarp::sig;
+using namespace yarp::os;
+
+int main(int argc, char *argv[]) 
+{
+    Network yarp;
+
+    Property params;
+    params.fromCommand(argc, argv);
+
+    if (!params.check("robot"))
+    {
+        fprintf(stderr, "Please specify the name of the robot\n");
+        fprintf(stderr, "--robot name (e.g. icub)\n");
+        return -1;
+    }
+    std::string robotName=params.find("robot").asString().c_str();
+    std::string remotePorts="/";
+    remotePorts+=robotName;
+    remotePorts+="/right_leg";
+
+    std::string localPorts="/codyco_deliv";
+
+    Property options;
+    options.put("device", "remote_controlboard");
+    options.put("local", localPorts.c_str());   //local port names
+    options.put("remote", remotePorts.c_str());         //where we connect to
+
+    // create a device
+    PolyDriver robotDevice(options);
+    if (!robotDevice.isValid()) {
+        printf("Device not available.  Here are the known devices:\n");
+        printf("%s", Drivers::factory().toString().c_str());
+        return 0;
+    }
+
+    IControlMode *controlMode;
+    IOpenLoopControl *pwm;
+
+    bool ok;
+    ok = robotDevice.view(pwm);
+    ok = ok && robotDevice.view(controlMode);
+
+    if (!ok) {
+        printf("Problems acquiring interfaces\n");
+        return 0;
+    }
+
+    int hip = 0, knee = 3;
+
+    controlMode->setOpenLoopMode(hip);
+    controlMode->setOpenLoopMode(knee);
+
+    pwm->setOutput(hip, 0);
+    pwm->setOutput(knee, 0);
+
+    robotDevice.close();
+    
+    return 0;
+}
