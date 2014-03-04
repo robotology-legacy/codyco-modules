267f7eb619f17f7e8259514d996c6d6d01987f8e
jeljaik <jorhabib@gmail.com>
2014-03-04 16:43:10 +0100

    fixed bug in inverseDynamics in Simulink

--- src/simulink/startup_wbclib.m
+++ src/simulink/startup_wbclib.m
@@ -5,12 +5,17 @@ if exist(wbclibroot,'dir')
     controllersDir = [wbclibroot filesep 'controllers'];
     addpath(controllersDir);
     cd(controllersDir);
-    libDir = '../../../build/lib';
+    cd ../../../build/lib;
+    libDir = pwd;
     imgDir = [controllersDir filesep 'img'];
-    addpath(libDir); %Path where mex libs are compiled
+    addpath(libDir);        %Path where mex libs are compiled
     addpath(imgDir);
     delete([pwd filesep 'pathdef.m'])
+    cd(controllersDir);     % Go back to controllers dir
     if (~savepath([pwd filesep 'pathdef.m']))
+        usrpath = userpath;
+        usrpath(end)=[];   
+%         copyfile([controllersDir '']);
         disp(sprintf('ATTENTION: In the current directory you should now see a file called pathdef.m. \nIf this file is not empty please replace your Matlab pathdef.m in'));
         disp([matlabroot '/toolbox/local' ' with this new file']); 
         disp(sprintf('Due to permissions restrictions this step must be done by the user.'));
