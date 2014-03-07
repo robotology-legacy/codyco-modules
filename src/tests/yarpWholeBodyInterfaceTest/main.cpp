79a288091b53f453e0326873f4e31a4d33b6c94a
francesco-romano <francesco.romano.1987@gmail.com>
2014-03-07 17:17:27 +0100

    consistency check for wbi

--- src/tests/yarpWholeBodyInterfaceTest/main.cpp
+++ src/tests/yarpWholeBodyInterfaceTest/main.cpp
@@ -45,15 +45,15 @@ const double TOL = 1e-8;
 
 int main(int argc, char * argv[])
 {
-    Network yarp; 
+    Network yarp;
     Property options;
     options.fromCommand(argc,argv);
     
     std::string robotName;
     if(options.check("robot")) {
-      robotName = options.find("robot").asString();
+        robotName = options.find("robot").asString();
     } else {
-      robotName = "double_pendulum";
+        robotName = "double_pendulum";
     }
     
     std::string urdfFile;
@@ -87,7 +87,7 @@ int main(int argc, char * argv[])
     double_pendulum->addJoints(LocalIdList(0,0,1));
     double_pendulum_sensor->addSensors(wbi::SENSOR_ENCODER,LocalIdList(0,0,1));
     std::cout << "Joints added, calling init method" <<  std::endl;
-
+    
     if(!double_pendulum->init())
         return -1;
     
@@ -101,49 +101,166 @@ int main(int argc, char * argv[])
     printf("Number of DoFs: %d\n", dof);
     
     Vector q(dof), dq(dof), d2q(dof);
-    double_pendulum_sensor->readSensors(wbi::SENSOR_ENCODER, q.data(),0,true);
-    Vector refSpeed(dof, CTRL_DEG2RAD*10.0), qd = q;
-    qd += 15.0*CTRL_DEG2RAD;
-    printf("Q:   %s\n", (CTRL_RAD2DEG*q).toString(1).c_str());
-    printf("Qd:  %s\n", (CTRL_RAD2DEG*qd).toString(1).c_str());
-    double_pendulum->setControlParam(CTRL_PARAM_REF_VEL, refSpeed.data());
-    double_pendulum->setControlReference(qd.data());
     
-    int j = 0;
-
-    for(int i=0; i<30; i++)
+    double_pendulum_sensor->readSensors(wbi::SENSOR_ENCODER, q.data(),0,true);
+//    Vector refSpeed(dof, CTRL_DEG2RAD*10.0), qd = q;
+//    qd += 15.0*CTRL_DEG2RAD;
+    printf("Q:   %s\n", (CTRL_RAD2DEG*q).toString(1).c_str());
+//    printf("Qd:  %s\n", (CTRL_RAD2DEG*qd).toString(1).c_str());
+//    double_pendulum->setControlParam(CTRL_PARAM_REF_VEL, refSpeed.data());
+    
+    dq.zero();
+    d2q.zero();
+    //    double_pendulum->setControlReference(qd.data());
+    //
+    //    int j = 0;
+    //
+    //    for(int i=0; i<30; i++)
+    //    {
+    //        Vector com(7,0.0);
+    //        wbi::Frame world2base;
+    //        world2base.identity();
+    //
+    //        Time::delay(1);
+    //        double_pendulum_sensor->readSensors(SENSOR_ENCODER, q.data());
+    //        printf("(Q):   %.2f \n", CTRL_RAD2DEG*q(j));
+    //
+    //        yarp::sig::Matrix mass_matrix(6+dof,6+dof);
+    //        mass_matrix.zero();
+    //
+    //        wbi::Frame id = wbi::Frame::identity();
+    //        double_pendulum->computeMassMatrix(q.data(),id,mass_matrix.data());
+    //
+    //        std::cout << "Joint mass matrix: " << std::endl;
+    //        std::cout << mass_matrix.submatrix(6,6+dof-1,6,6+dof-1).toString() << std::endl;
+    //    }
+    //
+    //    printf("Q:   %s\n", (CTRL_RAD2DEG*q).toString(1).c_str());
+    //
+    //    qd -= CTRL_DEG2RAD*15.0;
+    //    double_pendulum->setControlReference(qd.data());
+    
+    assert(dof == 2);
+    
+    //parameters
+    const double m1  = 1;
+    const double m2  = 1;
+    const double l1  = 0.5;
+    const double l2  = 0.5;
+    const double a1  = 1;
+    const double Il1 = 1.0/12.0;
+    const double Il2 = 1.0/12.0;
+    
+    double tau1 = 0;
+    double tau2 = 0;
+    
+    double readTau1 = 0;
+    double readTau2 = 0;
+    
+    double grav1 = 0;
+    double grav2 = 0;
+    
+    double cH = 0;
+    double cTerm1 = 0;
+    double cTerm2 = 0;
+    
+    double m11 = 0;
+    double m12 = 0;
+    double m22 = 0;
+    
+    double q1 = q(0);
+    double q2 = q(1);
+    double dq1 = 0;
+    double dq2 = 0;
+    
+    const double g = 9.8;
+    
+    grav1 = (m1 * l1 + m2 * a1) * g * cos(q1) + m2 * l2 * g * cos(q1 + q2);
+    grav2 = m2 * l2 * g * cos(q1 + q2);
+    
+    cH = -m2 * a1 * l2 * sin(q2);
+    cTerm1 = cH * dq2 * dq1 + cH * (dq1 + dq2) * dq2;
+    cTerm2 = -cH * dq1 * dq1;
+    
+    m11 = Il1 + m1 * l1 * l1 + Il2 + m2 * (a1 * a1 + l2 * l2 + 2 * a1* l2 * cos(q2));
+    m12 = Il2 + m2 * (l2 * l2 + a1* l2 * cos(q2));
+    m22 = Il2 + m2 * l2 * l2;
+    
+    yarp::sig::Matrix M(6 + dof, 6 + dof);
+//    M(0,0) = m11;
+//    M(0,1) = M(1,0) = m12;
+//    M(1,1) = m22;
+    
+    yarp::sig::Vector Cdq(6 + dof);
+//    Cdq(0) = cTerm1;
+//    Cdq(1) = cTerm2;
+    
+    yarp::sig::Vector grav(dof + 6);
+//    grav(0) = grav1;
+//    grav(1) = grav2;
+    yarp::sig::Vector tempTau(dof + 6);
+    yarp::sig::Vector zeroVec(dof + 6);
+    zeroVec.zero();
+    
+    Frame xBase = Frame::identity();
+    //try the base to coicide with the origin of the upper link
+    double baseVelocity[6] = {0, 0, 0, 0, 0, 0};
+    double baseAcc[6] = {0, 0, 0, 0, 0, 0};
+    double gravity[3] = {0, 0, -g};
+    
+    if (!double_pendulum->inverseDynamics(q.data(), xBase, zeroVec.data(), baseVelocity, zeroVec.data(), baseAcc, gravity, grav.data())) {
+        fprintf(stderr, "Inverse Dynamics failed \n");
+    }
+    if (grav1 != grav(6 + 0)) {
+        fprintf(stderr, "Gravity is %lf but should be %lf\n", grav(6), grav1);
+    }
+    if(grav2 != grav(6 + 1))
     {
-        Vector com(7,0.0);
-        wbi::Frame world2base;
-        world2base.identity();
-        
-        Time::delay(1);
-        double_pendulum_sensor->readSensors(SENSOR_ENCODER, q.data());
-        printf("(Q):   %.2f \n", CTRL_RAD2DEG*q(j));
-        
-        yarp::sig::Matrix mass_matrix(6+dof,6+dof);
-        mass_matrix.zero();
-        
-        wbi::Frame id = wbi::Frame::identity();
-        double_pendulum->computeMassMatrix(q.data(),id,mass_matrix.data());
-        
-        std::cout << "Joint mass matrix: " << std::endl;
-        std::cout << mass_matrix.submatrix(6,6+dof-1,6,6+dof-1).toString() << std::endl;
+        fprintf(stderr, "Gravity is %lf but should be %lf\n", grav(7), grav2);
     }
     
-    printf("Q:   %s\n", (CTRL_RAD2DEG*q).toString(1).c_str());
+    if(!double_pendulum->inverseDynamics(q.data(), xBase, dq.data(), baseVelocity, zeroVec.data(), baseAcc, gravity, tempTau.data())) {
+            fprintf(stderr, "Inverse Dynamics failed \n");
+    }
+    Cdq = tempTau - grav;
+    if (cTerm1 != Cdq(6 + 0)) {
+        fprintf(stderr, "C term is %lf but should be %lf\n", Cdq(6), cTerm1);
+    }
+    if(cTerm2 != Cdq(6 + 1))
+    {
+        fprintf(stderr, "C Term is %lf but should be %lf\n", Cdq(7), cTerm2);
+    }
+    
+    if(!double_pendulum->computeMassMatrix(q.data(), xBase, M.data())) {
+        fprintf(stderr, "Mass Matrix failed \n");
+    }
+    if(m11 != M(6 + 0,6 + 0))
+    {
+        fprintf(stderr, "Mass matrix term 0,0 is %lf but should be %lf\n", M(6 + 0,6 + 0), m11);
+    }
+    if(m12 != M(6 + 0, 6 + 1))
+    {
+        fprintf(stderr, "Mass matrix term 0,1 is %lf but should be %lf\n", M(6 + 1,6 + 0), m12);
+    }
+    if(m12 != M(6 + 1, 6 + 0))
+    {
+        fprintf(stderr, "Mass matrix term 1,0 is %lf but should be %lf\n", M(6 + 0,6 + 1), m12);
+    }
+    if(m22 != M(6 + 1, 6 + 1))
+    {
+    fprintf(stderr, "Mass matrix term 1,1 is %lf but should be %lf\n",M(6 + 1,6 + 1), m22);
+    }
+    
 
-    qd -= CTRL_DEG2RAD*15.0;
-    double_pendulum->setControlReference(qd.data());
-
+    
     Time::delay(1.0);
     printf("Test finished..");
     
-    double_pendulum->close();
     double_pendulum_sensor->close();
+    double_pendulum->close();
     
-    delete double_pendulum;
     delete double_pendulum_sensor;
+    delete double_pendulum;
     
     printf("Main returning...\n");
     return 0;
