464ea1ad3171c0e90335bc9b89dc8d61f910ed2d
francesco-romano <francesco.romano.1987@gmail.com>
2014-03-11 15:48:23 +0100

    Fixed iCub WBI CMake. JointTorqueControl: enable again offset

--- src/modules/jointTorqueControl/src/jointTorqueControlThread.cpp
+++ src/modules/jointTorqueControl/src/jointTorqueControlThread.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2013 CoDyCo
  * Author: Daniele Pucci
  * email:  daniele.pucci@iit.it
@@ -13,7 +13,7 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details
-*/
+ */
 
 #include <jointTorqueControl/jointTorqueControlThread.h>
 #include <jointTorqueControl/jointTorqueControlConstants.h>
@@ -26,8 +26,8 @@ using namespace wbiIcub;
 
 
 jointTorqueControlThread::jointTorqueControlThread(int period, string _name, string _robotName, ParamHelperServer *_ph, wholeBodyInterface *_wbi)
-    : RateThread(period), name(_name), robotName(_robotName), paramHelper(_ph), robot(_wbi), sendCommands(SEND_COMMANDS_NONACTIVE),
-    monitoredJointId(0)
+: RateThread(period), name(_name), robotName(_robotName), paramHelper(_ph), robot(_wbi), sendCommands(SEND_COMMANDS_NONACTIVE),
+monitoredJointId(0)
 {
     mustStop = false;
     status = CONTROL_OFF;
@@ -55,7 +55,7 @@ bool jointTorqueControlThread::threadInit()
     YARP_ASSERT(paramHelper->linkParam(PARAM_ID_VMAX,	            Vmax.data()));
     YARP_ASSERT(paramHelper->linkParam(PARAM_ID_SENDCMD,            &sendCommands));
 	YARP_ASSERT(paramHelper->linkParam(PARAM_ID_MONITORED_JOINT,    &monitoredJointName));
-
+    
     // link controller input streaming parameters to member variables
     YARP_ASSERT(paramHelper->linkParam(PARAM_ID_TAU_OFFSET,	        tauOffset.data()));
     YARP_ASSERT(paramHelper->linkParam(PARAM_ID_TAU_SIN_AMPL,	    tauSinAmpl.data()));
@@ -80,45 +80,45 @@ bool jointTorqueControlThread::threadInit()
     YARP_ASSERT(paramHelper->linkParam(PARAM_ID_PWM_FEEDBACK,	    &monitor.pwmFB));
     YARP_ASSERT(paramHelper->linkParam(PARAM_ID_PWM_TORQUE_FF,	    &monitor.pwmTorqueFF));
     YARP_ASSERT(paramHelper->linkParam(PARAM_ID_PWM_FRICTION_FF,	&monitor.pwmFrictionFF));
-
+    
     // Register callbacks for some module parameters
     YARP_ASSERT(paramHelper->registerParamValueChangedCallback(PARAM_ID_AJ,                 this));
 	YARP_ASSERT(paramHelper->registerParamValueChangedCallback(PARAM_ID_SENDCMD,            this));
     YARP_ASSERT(paramHelper->registerParamValueChangedCallback(PARAM_ID_MONITORED_JOINT,    this));
-
+    
     // Register callbacks for some module commands
     YARP_ASSERT(paramHelper->registerCommandCallback(COMMAND_ID_START,           this));
     YARP_ASSERT(paramHelper->registerCommandCallback(COMMAND_ID_STOP,            this));
-
-    tau 			= VectorNd::Constant(0.0); 
-	etau 			= VectorNd::Constant(0.0); 
-	tauD 			= VectorNd::Constant(0.0); 
-    tauOffset 		= VectorNd::Constant(0.0);  
-	integralState 	= VectorNd::Constant(0.0); 
+    
+    tau 			= VectorNd::Constant(0.0);
+	etau 			= VectorNd::Constant(0.0);
+	tauD 			= VectorNd::Constant(0.0);
+    tauOffset 		= VectorNd::Constant(0.0);
+	integralState 	= VectorNd::Constant(0.0);
 	motorVoltage	= VectorNd::Constant(0.0);
     dqSign          = VectorNd::Constant(0.0);
-
+    
     ///< thread constants
     zeroN.setZero();
     zero6[0] = zero6[1] = zero6[2] = zero6[3] = zero6[4] = zero6[5] = 0.0;
     ddxB[0] = ddxB[1] = ddxB[3] = ddxB[4] = ddxB[5] = 0.0;
     ddxB[2] = 9.81;     ///< gravity acceleration
-
+    
     activeJointsOld = activeJoints;
-
+    
     if(!updateJointToMonitor())
         printf("Specified monitored joint name was not recognized: %s\n", monitoredJointName.c_str());
-
+    
     double torqueFiltCutFreq = 1.0;
     robot->setEstimationParameter(ESTIMATE_JOINT_VEL,    ESTIMATION_PARAM_ADAPTIVE_WINDOW_MAX_SIZE, &JOINT_VEL_ESTIMATION_WINDOW);
     robot->setEstimationParameter(ESTIMATE_JOINT_TORQUE, ESTIMATION_PARAM_LOW_PASS_FILTER_CUT_FREQ, &torqueFiltCutFreq);
-
+    
     if(!readRobotStatus(true))
     {
         printf("Error while initializing the controller: it was not possible to read the robot status!\n");
         return false;
     }
-
+    
     qDes = q;
     
     return true;
@@ -136,39 +136,39 @@ void jointTorqueControlThread::run()
 		double dt           = currentTime - oldTime;
 		
 		readRobotStatus(false);
-
+        
 		for (int i=0; i < N_DOF; i++)
         {
-            dqSign(i)       = fabs(dq(i))>coulombVelThr(i) ? sign(dq(i)) : pow(dq(i)/coulombVelThr(i),3);
+            dqSign(i) = fabs(dq(i))>coulombVelThr(i) ? sign(dq(i)) : pow(dq(i)/coulombVelThr(i),3);
             //tauD(i)         = ks(i)*(qDes(i)-q(i)) - kd(i)*dq(i) + gravityCompOn*tauGrav(i+6);
-            tauD(i)        = 0;//tauOffset(i);// + tauSinAmpl(i)*sin(2*M_PI*tauSinFreq(i)*currentTime);
-
-			if (activeJoints(i) == 1) 
+            tauD(i) = tauOffset(i);// + tauSinAmpl(i)*sin(2*M_PI*tauSinFreq(i)*currentTime);
+            
+			if (activeJoints(i) == 1)
             {
 				etau(i) 			= tauM(i) - tauD(i);
 				integralState(i) 	= saturation(integralState(i) + ki(i)*dt*etau(i), TORQUE_INTEGRAL_SATURATION, -TORQUE_INTEGRAL_SATURATION) ;
 				tau(i) 				= tauD(i) - kp(i)*etau(i) - integralState(i);
-
+                
                 if(dq(i)>0)
                     motorVoltage(i) = kt(i)*tau(i) + kvp(i)*dq(i) + kcp(i)*dqSign(i);
                 else
                     motorVoltage(i) = kt(i)*tau(i) + kvn(i)*dq(i) + kcn(i)*dqSign(i);
-
-		if (sendCommands == SEND_COMMANDS_ACTIVE) {
-			robot->setControlReference(&motorVoltage(i), i);
+                
+                if (sendCommands == SEND_COMMANDS_ACTIVE) {
+                    robot->setControlReference(&motorVoltage(i), i);
                     //robot->setControlParam(wbi::CTRL_PARAM_OFFSET, &motorVoltage(i), i);
                 }
-	    }
-	}
-
+            }
+        }
+        
 		oldTime = currentTime;
     }
-
+    
     prepareMonitorData();
     
     paramHelper->sendStreamParams();
     paramHelper->unlock();
-
+    
     printCountdown = (printCountdown>=PRINT_PERIOD) ? 0 : printCountdown +(int)getRate();   // countdown for next print (see sendMsg method)
 }
 
@@ -181,7 +181,7 @@ bool jointTorqueControlThread::readRobotStatus(bool blockingRead)
     res = res && robot->getEstimates(ESTIMATE_JOINT_TORQUE, tauM.data(),    -1.0, blockingRead);
     res = res && robot->getEstimates(ESTIMATE_MOTOR_PWM,    pwmMeas.data(), -1.0, blockingRead);
     res = res && robot->inverseDynamics(q.data(), Frame(), zeroN.data(), zero6, zeroN.data(), ddxB, zero6, tauGrav.data());
-
+    
     // convert angles from rad to deg
     q   *= CTRL_RAD2DEG;
     dq  *= CTRL_RAD2DEG;
@@ -195,7 +195,7 @@ void jointTorqueControlThread::startSending()
     status = CONTROL_ON;       //sets thread status to ON
 	setControlModePWMOnJoints(sendCommands == SEND_COMMANDS_ACTIVE);
     oldTime = yarp::os::Time::now();
-    printf("Activating the torque control.\n"); 
+    printf("Activating the torque control.\n");
 }
 
 //*************************************************************************************************************************
@@ -214,10 +214,10 @@ void jointTorqueControlThread::resetIntegralState(int j)
         integralState(j) = 0.0;
         return;
     }
-
+    
 	for (int i=0; i < N_DOF; i++)
-		if (activeJoints(i) == 1) 
-			integralState(i) = 0.0; 
+		if (activeJoints(i) == 1)
+			integralState(i) = 0.0;
 }
 
 //*************************************************************************************************************************
@@ -241,18 +241,18 @@ void jointTorqueControlThread::parameterUpdated(const ParamProxyInterface *pd)
 {
     switch(pd->id)
     {
-    case PARAM_ID_AJ:
-        activeJointsChanged();
-        break;
-	case PARAM_ID_SENDCMD:
-		//setControlModePWMOnJoints(sendCommands == SEND_COMMANDS_ACTIVE);
-		break;
-    case PARAM_ID_MONITORED_JOINT:
-        if(!updateJointToMonitor())
-            printf("Specified monitored joint name was not recognized: %s\n", monitoredJointName.c_str());
-        break;
-    default:
-        printf("A callback is registered but not managed for the parameter %s\n", pd->name.c_str());
+        case PARAM_ID_AJ:
+            activeJointsChanged();
+            break;
+        case PARAM_ID_SENDCMD:
+            //setControlModePWMOnJoints(sendCommands == SEND_COMMANDS_ACTIVE);
+            break;
+        case PARAM_ID_MONITORED_JOINT:
+            if(!updateJointToMonitor())
+                printf("Specified monitored joint name was not recognized: %s\n", monitoredJointName.c_str());
+            break;
+        default:
+            printf("A callback is registered but not managed for the parameter %s\n", pd->name.c_str());
     }
 }
 
@@ -261,9 +261,9 @@ void jointTorqueControlThread::commandReceived(const CommandDescription &cd, con
 {
     switch(cd.id)
     {
-    case COMMAND_ID_START:  startSending(); break;
-    case COMMAND_ID_STOP:   stopSending();  break;
-    default:    printf("A callback is registered but not managed for the command %s\n", cd.name.c_str());
+        case COMMAND_ID_START:  startSending(); break;
+        case COMMAND_ID_STOP:   stopSending();  break;
+        default:    printf("A callback is registered but not managed for the command %s\n", cd.name.c_str());
     }
 }
 
@@ -277,7 +277,7 @@ bool jointTorqueControlThread::activeJointsChanged()
         {
             resetIntegralState(i);
             if(status==CONTROL_ON && sendCommands == SEND_COMMANDS_ACTIVE) {
-		robot->setControlMode(CTRL_MODE_MOTOR_PWM, NULL, i);
+                robot->setControlMode(CTRL_MODE_MOTOR_PWM, NULL, i);
                 //robot->setControlMode(CTRL_MODE_TORQUE, NULL, i);
                 //robot->setControlParam(wbi::CTRL_PARAM_KP, &zeroValue); //why I didn't used the function "setControlModePWMOnJoints"?
             }
@@ -326,7 +326,7 @@ int jointTorqueControlThread::convertGlobalToLocalJointId(const Bottle &b)
 {
     if(b.size()==0)
         return -1;
-
+    
     LocalId lid;
     if(b.get(0).isString())
     {
@@ -341,7 +341,7 @@ int jointTorqueControlThread::convertGlobalToLocalJointId(const Bottle &b)
     
     if(lid.bodyPart==iCub::skinDynLib::BODY_PART_UNKNOWN)
         return -1;
-
+    
     int jid = robot->getJointList().localToGlobalId(lid);
     return jid;
 }
