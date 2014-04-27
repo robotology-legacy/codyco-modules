c87b92c1904c7aa12bc6f582fd8d7979926ba02a
icub <silvio.traversaro@iit.it>
2014-04-27 21:34:56 +0200

    Configuration files

--- src/simulink/controllers/torqueBalancing/initSharedMem.m
+++ src/simulink/controllers/torqueBalancing/initSharedMem.m
@@ -13,8 +13,14 @@ Ts = 0.01;
 % Controller gains in P I D order
 weights = [0.2512      0.3335       0.428      0.9506      0.9604      0.9831      0.9877      0.9974      0.9514      0.9601      0.9829      0.9879      0.9974      0.4056       0.213      0.2676      0.1629      0.0545      0.0108      0.4054       0.213      0.2674      0.1628      0.0545      0.0108];
 
-k_com = [ 20    5   15];
-k_pst = [ 100.*diag(1./weights)   0.*diag(1./weights)   0.*diag(1./weights)];
-k_pos = [ 100   500   10];
+k_com = [ 50    0   0];
+k_pst = [ 100*eye(25)    0*eye(25)   0*eye(25)];   
+k_pst(1,1) = 2800;
+k_pos = [ 10    0   0];
   
-kw = 1;  
+kw = 1;
+
+% Rotation of the gazebo FT sensor
+R   = [0 0 1; 0 -1 0;1 0 0];
+Rf  = [R, zeros(3,3); zeros(3,3), R];
+
