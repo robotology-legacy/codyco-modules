42328604c41ff77e2f11b22cf69bde3c135fe185
Daniele Pucci <daniele.pucci@iit.it>
2014-05-02 15:03:04 +0200

    Working on the balancing for the real robot

--- src/simulink/controllers/torqueBalancing/initRov14.m
+++ src/simulink/controllers/torqueBalancing/initRov14.m
@@ -8,9 +8,10 @@ addpath(genpath('/home/daniele/src/codyco/src/simulink'))
 Ts = 0.01; 
  
 % Controller gains in P I D order
-k = [  10    0.1   2
-       550    0    1
-       500    0  20 ];
-   
+k  = [  10    0.0   2 ];
+ko = [ 100 100 100      200 200 200 200 200     20 20 20 20 20 20; 
+         1   1   1        1   1   1   1   1      1  1  1  1  1  1]; 
+kSat = [500 500 500 3000 3000 3000];  
+
 % 
 fake = eye(25);
