29b0b88bcc1880c844571f6a9bcee089e3ce48a8
Francesco Nori <francesco.nori@iit.it>
2014-05-06 11:30:08 +0200

    controller to be tested

--- src/simulink/controllers/torqueBalancing/initPartialForceControl.m
+++ src/simulink/controllers/torqueBalancing/initPartialForceControl.m
@@ -10,16 +10,16 @@ nf = 6;    %number of controlled contraints
 % Controller period
 Ts = 0.01; 
 
-k_pst_torso = [0 0 0];
-k_pst_arm   = [0 0 0 0 0];
-k_pst_leg   = [0 0 0 0 0 0];
+k_pst_torso = [600 600 100]*0;
+k_pst_arm   = [200 200 200 200 200]*0;
+k_pst_leg   = [200 200 200 200 200  200]*0;
 
-k_f_linear  = [0 0 0];
-k_f_rotatio = [0 0 0];
+k_f_linear  = [0.050 0.050 0.050];
+k_f_rotatio = [0.008 0.008 0.008];
 
 k_f   = diag([k_f_linear k_f_rotatio]);
 k_pst = [ diag([k_pst_torso k_pst_arm k_pst_arm k_pst_leg k_pst_leg])    0*eye(25)   0*eye(25)];   
 
-% Rotation of the gazebo FT sensor
-R   = [1 0 0; 0 -1 0;0 0 -1];
+% Rotation of the real robot FT sensor
+R   = [0 0 1; 0 -1 0;1 0 0];
 Rf  = [R, zeros(3,3); zeros(3,3), R];
