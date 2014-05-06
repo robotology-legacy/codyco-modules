5eae9c7a593b458a37cc1ce130d3a3d155061f22
Silvio Traversaro <silvio.traversaro@iit.it>
2014-05-07 00:48:34 +0200

    Update coordinator script

--- src/modules/codycoCoordinatorDemo1Y/codycoCoordinatorDemo1Y.lua
+++ src/modules/codycoCoordinatorDemo1Y/codycoCoordinatorDemo1Y.lua
@@ -11,7 +11,7 @@ yarp.Network()
 event_no_contact = "e_no_contacts_on_hands"
 event_contact_on_left_hand = "e_contacts_only_on_left_hand"
 event_contact_on_right_hand = "e_contacts_only_on_right_hand"
-event_no_contact = "e_contacts_on_both_hands"
+event_contacts_on_both_hands = "e_contacts_on_both_hands"
 
 ----definitions
 ----copied from skinDynLib common.h
@@ -21,16 +21,40 @@ bodyPart_right_arm = 4
 -------
 function update_skin_events()
    while true do
+        --Use last received skinContactsList
         skin_contacts = event_port:read(false)
         if skin_contacts ~= nil then
             buffer_skin_contacts = skin_contacts
         end
+
+        --Count contacts on left_arm and right_arm
+        contact_left_arm = 0
+        contact_right_arm = 0
         for i = 1,buffer_skin_contacts:size() do
-            buffer_skin_contacts.get(i).get(0).get()
+            bp_contact = buffer_skin_contacts:get(i):asList():get(0):asList():get(1)
+            if( bp_contact == bodyPart_left_arm ) then
+                contact_left_arm = contact_left_arm + 1
+            end
+            if( bp_contact == bodyPart_right_arm ) then
+                contact_right_arm = contact_right_arm + 1
+            end
         end
-        -- print('Received ', event:toString())
-        -- read skin events and produce proper evnets
-        rfsm.send_events(fsm, )
+
+        -- Produce proper events given the contact state
+        if( contact_left_arm > 0 and contact_right_arm > 0 ) then
+            event_to_send = event_contacts_on_both_hands
+        end
+        if( contact_left_arm == 0 and contact_right_arm > 0 ) then
+            event_to_send = event_contact_on_right_hand
+        end
+        if( contact_left_arm > 0 and contact_right_arm == 0 ) then
+            event_to_send = event_contact_on_left_hand
+        end
+        if( contact_left_arm == 0 and contact_right_arm == 0 ) then
+            event_to_send = event_no_contact
+        end
+
+        rfsm.send_events(fsm, event_to_send)
         if coroutine.yield() == true then break end
    end
 end
