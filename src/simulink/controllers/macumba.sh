4af7da5ef29c8e824fe3d49a1f9920bd224b40bf
Daniele Pucci <daniele.pucci@iit.it>
2014-05-08 09:43:11 +0200

    updates on balancing for demo CoDYCo

--- src/simulink/controllers/macumba.sh
+++ src/simulink/controllers/macumba.sh
@@ -14,12 +14,13 @@ for a in $list; do
 	echo "set aen $a"  | yarp rpc /icub/left_leg/rpc:i
 done
 
-sleep 1
-
 for a in $list; do
 	echo "set ena $a"  | yarp rpc /icub/right_leg/rpc:i
 	echo "set ena $a"  | yarp rpc /icub/left_leg/rpc:i
 done
+
+list="1"
+
 for a in $list; do
 	echo "set adi $a"  | yarp rpc /icub/right_leg/rpc:i
 	echo "set adi $a"  | yarp rpc /icub/left_leg/rpc:i
@@ -35,5 +36,10 @@ for a in $list; do
 	echo "set ena $a"  | yarp rpc /icub/right_leg/rpc:i
 	echo "set ena $a"  | yarp rpc /icub/left_leg/rpc:i
 done
+
+echo "!!Now I will calib the wholeBodyDynamics!!"
+sleep 5
+echo "calibStanding all 200"  | yarp rpc /wholeBodyDynamicsTree/rpc:i
+
 
 
