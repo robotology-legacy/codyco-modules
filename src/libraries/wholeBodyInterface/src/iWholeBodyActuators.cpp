5daaa50eecaa182578b4f33ca14d4e38fd81212d
Silvio Traversaro <silvio.traversaro@iit.it>
2014-05-21 12:34:43 +0200

    Removed libraries moved to external repositories

--- src/libraries/wholeBodyInterface/src/iWholeBodyActuators.cpp
+++
@@ -1,3 +0,0 @@
-#include <wbi/iWholeBodyActuators.h>
-
-wbi::iWholeBodyActuators::~iWholeBodyActuators() {}
--- src/libraries/wholeBodyInterface/src/iWholeBodyStates.cpp
+++
@@ -1,3 +0,0 @@
-#include <wbi/iWholeBodyStates.h>
-
-wbi::iWholeBodyStates::~iWholeBodyStates() {}
