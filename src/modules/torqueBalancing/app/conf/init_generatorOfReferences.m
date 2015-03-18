07c92efd8a750c255f1f89c2384a3815d31dc228
iCub <iCub@iit.it>
2015-03-18 15:11:11 +0100

    Added generator for references of torque balancing

---
+++ src/modules/torqueBalancing/app/conf/init_generatorOfReferences.m
@@ -0,0 +1,20 @@
+noOscillationTime        = 10;
+
+robotName = 'icub';
+localName = 'refGen4TorqueBalancing';
+
+outputPortCoM       = ['/', localName, '/com:o'];
+outputPortPostural  = ['/', localName, '/qDes:o'];
+balancingPort       = 'myTest';
+
+
+% Controller period
+Ts                = 0.01;
+simulationTime    = inf;
+
+ROBOT_DOF = 23;
+directionOfOscillation  = [0;1;0];
+referenceParamsCoM      = [0.04  0.05];  %referenceParams(1) = amplitude of ascillations in meters referenceParams(2) = frequency of ascillations in hertz
+referenceParamsPost     = [20*pi/180  0.05];           
+mask                    = zeros(ROBOT_DOF ,1);
+mask(7)                 = 1;                    
