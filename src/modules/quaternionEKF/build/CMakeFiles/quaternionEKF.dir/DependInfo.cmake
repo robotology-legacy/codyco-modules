799b0e5a0e7653c7a0dc438f311725dd86370b76
jorhabib <jorhabib@gmail.com>
2015-06-11 13:54:22 +0200

    Added quaternionEKF module

---
+++ src/modules/quaternionEKF/build/CMakeFiles/quaternionEKF.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/src/dataDumperParser.cpp" "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/build/CMakeFiles/quaternionEKF.dir/src/dataDumperParser.cpp.o"
+  "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/src/deviceclass.cpp" "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/build/CMakeFiles/quaternionEKF.dir/src/deviceclass.cpp.o"
+  "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/src/main.cpp" "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/build/CMakeFiles/quaternionEKF.dir/src/main.cpp.o"
+  "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/src/nonLinearAnalyticConditionalGaussian.cpp" "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/build/CMakeFiles/quaternionEKF.dir/src/nonLinearAnalyticConditionalGaussian.cpp.o"
+  "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/src/nonLinearMeasurementGaussianPdf.cpp" "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/build/CMakeFiles/quaternionEKF.dir/src/nonLinearMeasurementGaussianPdf.cpp.o"
+  "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/src/quaternionEKFModule.cpp" "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/build/CMakeFiles/quaternionEKF.dir/src/quaternionEKFModule.cpp.o"
+  "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/src/quaternionEKFThread.cpp" "/home/jorhabib/Software/extended-kalman-filter/EKF_Quaternion_DynWalking2015/orocos_bfl/build/CMakeFiles/quaternionEKF.dir/src/quaternionEKFThread.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "YARP_NO_DEPRECATED_WARNINGS"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/jorhabib/Software/yarp/build/generated_include"
+  "/home/jorhabib/Software/yarp/src/libYARP_OS/include"
+  "/home/jorhabib/Software/yarp/src/libYARP_sig/include"
+  "/home/jorhabib/Software/yarp/src/libYARP_math/include"
+  "/home/jorhabib/Software/yarp/src/libYARP_dev/include"
+  "/home/jorhabib/Software/yarp/src/libYARP_name/include"
+  "/home/jorhabib/Software/yarp/src/libYARP_manager/include"
+  "/home/jorhabib/Software/yarp/src/libYARP_logger/include"
+  "/home/jorhabib/Software/yarp/src/yarpserver/include"
+  "/usr/local/include"
+  "../include"
+  "/usr/local/xsens/public/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ src/modules/quaternionEKF/build/CMakeFiles/quaternionEKF.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/quaternionEKF.dir/src/deviceclass.cpp.o"
+  "CMakeFiles/quaternionEKF.dir/src/nonLinearMeasurementGaussianPdf.cpp.o"
+  "CMakeFiles/quaternionEKF.dir/src/nonLinearAnalyticConditionalGaussian.cpp.o"
+  "CMakeFiles/quaternionEKF.dir/src/quaternionEKFModule.cpp.o"
+  "CMakeFiles/quaternionEKF.dir/src/dataDumperParser.cpp.o"
+  "CMakeFiles/quaternionEKF.dir/src/quaternionEKFThread.cpp.o"
+  "CMakeFiles/quaternionEKF.dir/src/main.cpp.o"
+  "quaternionEKF.pdb"
+  "quaternionEKF"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/quaternionEKF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
