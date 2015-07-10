772cf5bf7dc9d84f018d8e88a5a02d1ad2f6a321
jorhabib <jorhabib@gmail.com>
2015-07-10 12:00:12 +0200

    missing files

---
+++ src/modules/quaternionEKF/include/xsensclasses.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2014 Fondazione Istituto Italiano di Tecnologia - Italian Institute of Technology
+ * Author: Jorhabib Eljaik
+ * email:  jorhabib.eljaik@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#ifndef __XSENSCLASSES_H__
+#define __XSENSCLASSES_H__
+
+#include <xsens/xsresultvalue.h>
+#include <xsens/xsbytearray.h>
+#include <xsens/xsmessagearray.h>
+#include <xsens/xsdeviceid.h>
+#include <xsens/xsportinfo.h>
+#include <xsens/xsoutputmode.h>
+#include <xsens/xsoutputsettings.h>
+#include <xsens/xsoutputconfigurationarray.h>
+
+#include <xsens/xsportinfoarray.h>
+#include <xsens/xsdatapacket.h>
+#include <xsens/xstime.h>
+#include <xcommunication/legacydatapacket.h>
+#include <xcommunication/int_xsdatapacket.h>
+#include <xcommunication/enumerateusbdevices.h>
+
+#include <xcommunication/protocolhandler.h>
+#include <xcommunication/usbinterface.h>
+#include <xcommunication/serialinterface.h>
+#include <xcommunication/streaminterface.h>
+
+#include "deviceclass.h"
+
+#endif
