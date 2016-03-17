4840255e666fed835ac9b028fb1d415f1521bc7c
Silvio <pegua1@gmail.com>
2016-03-17 15:56:19 +0100

    [cmake] Get rid of CODYCO_USES_EIGEN_320 option
    Ref https://github.com/robotology/codyco-superbuild/issues/96

--- cmake/modules/CoDyCoFindDependencies.cmake
+++ cmake/modules/CoDyCoFindDependencies.cmake
@@ -1,9 +1,5 @@
 #### Find Eigen3
-if (${CODYCO_USES_EIGEN_320})
-    find_package(Eigen3 3.2 REQUIRED)
-else()
-    find_package(Eigen3 3.0 REQUIRED)
-endif()
+find_package(Eigen3 3.2 REQUIRED)
 
 find_package(iDynTree 0.3.3 REQUIRED)
 find_package(paramHelp 0.0.3 REQUIRED)
