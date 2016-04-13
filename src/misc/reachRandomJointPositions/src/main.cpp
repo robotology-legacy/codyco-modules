b0fa4db193ac6ab7dc453d34f2cc1853b5d18ded
Silvio Traversaro <silvio.traversaro@iit.it>
2016-04-13 10:12:40 +0200

    cleanup reachRandomJointPositions

--- src/misc/reachRandomJointPositions/src/main.cpp
+++ src/misc/reachRandomJointPositions/src/main.cpp
@@ -39,7 +39,7 @@ int main (int argc, char * argv[])
     //Creating and preparing the Resource Finder
     ResourceFinder rf;
     rf.setVerbose(true);
-    rf.setDefaultConfigFile("randomJointsDefault.ini"); //default config file name.
+    rf.setDefaultConfigFile("randomJointsLeftLeg.ini"); //default config file name.
     rf.setDefaultContext("reachRandomJointPositions"); //when no parameters are given to the module this is the default context
     rf.configure(argc,argv);
 
