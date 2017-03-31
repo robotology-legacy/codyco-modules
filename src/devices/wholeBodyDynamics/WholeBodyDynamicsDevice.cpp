7cf5cc8bb5b0cce67f52ebce4f9f49e76f9430f3
Francesco Romano <francesco.romano.1987@gmail.com>
2017-03-31 20:59:38 +0200

    Added interrupt on ROS node closing

--- src/devices/wholeBodyDynamics/WholeBodyDynamicsDevice.cpp
+++ src/devices/wholeBodyDynamics/WholeBodyDynamicsDevice.cpp
@@ -1854,8 +1854,11 @@ bool WholeBodyDynamicsDevice::close()
     this->remappedControlBoard.close();
     this->remappedVirtualAnalogSensors.close();
 
-    delete publisherNode;
-    publisherNode = 0;
+    if (publisherNode) {
+        publisherNode->interrupt();
+        delete publisherNode;
+        publisherNode = 0;
+    }
 
     correctlyConfigured = false;
 
