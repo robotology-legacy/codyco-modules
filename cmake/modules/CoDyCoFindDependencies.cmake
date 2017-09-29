b7c7f7c81592c3a13c5b01e39db9b4ce0288bb5b
Silvio Traversaro <pegua1@gmail.com>
2017-09-29 13:15:58 +0200

    Bump iDynTree required version to 0.8.1
    See https://github.com/robotology/idyntree/pull/378

--- cmake/modules/CoDyCoFindDependencies.cmake
+++ cmake/modules/CoDyCoFindDependencies.cmake
@@ -1,7 +1,7 @@
 #### Find Eigen3
 find_package(Eigen3 3.2 REQUIRED)
 
-find_package(iDynTree 0.3.14 REQUIRED)
+find_package(iDynTree 0.8.1 REQUIRED)
 find_package(yarpWholeBodyInterface 0.3.5 REQUIRED)
 find_package(paramHelp 0.0.3 REQUIRED)
 find_package(TinyXML)
