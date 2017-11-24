c14dedac774a5898c024e663ed840d82e8a4f644
Silvio <pegua1@gmail.com>
2017-11-24 10:18:59 +0100

    Dependencies cleanup

--- cmake/modules/CoDyCoFindDependencies.cmake
+++ cmake/modules/CoDyCoFindDependencies.cmake
@@ -3,5 +3,3 @@ find_package(Eigen3 3.2 REQUIRED)
 
 find_package(iDynTree 0.8.1 REQUIRED)
 find_package(yarpWholeBodyInterface 0.3.5 REQUIRED)
-find_package(paramHelp 0.0.3 REQUIRED)
-find_package(TinyXML)
